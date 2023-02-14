cb1925f6f2fc54cba4592c77922c6691ca099fbc
doskdkfn <dawn_1216@naver.com>
2023-02-14 17:10:52 +0900

    2023-02-14 slide

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    var idx=0;
+    setInterval(function () {
+        $('#slide > a'), eq(idx).fadeOut(400);
+        idx==2?idx=0:idx++;
+        $('#slide > a').eq(idx).fadeIn(800);
+    }, 3000);
+});
