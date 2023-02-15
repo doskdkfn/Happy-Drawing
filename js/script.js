0dde5348212f3ed893f16652f141acd2aa751cdb
doskdkfn <dawn_1216@naver.com>
2023-02-15 17:27:39 +0900

    2023-02-15

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     var idx=0;
     setInterval(function () {
-        $('#slide > a'), eq(idx).fadeOut(400);
+        $('#slide > a'). eq(idx).fadeOut(400);
         idx==2?idx=0:idx++;
         $('#slide > a').eq(idx).fadeIn(800);
     }, 3000);
