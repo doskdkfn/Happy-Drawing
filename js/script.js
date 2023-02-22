d696f0eececf48b1f6d156a7c3315015df3de516
doskdkfn <dawn_1216@naver.com>
2023-02-22 17:22:12 +0900

    2023-02-22 gallery-big

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 $(document).ready(function () {
+
+ /************ main-slide ***********/
+
     var idx=0;
     setInterval(function () {
         $('#slide > a'). eq(idx).fadeOut(400);
         idx==2?idx=0:idx++;
         $('#slide > a').eq(idx).fadeIn(800);
     }, 3000);
+
+ /*********** gallery-photo **********/
+
+    $('.thumb_item').click(function () {
+        $('#photo-back,#big,.photo-close').fadeIn();
+    });
+
+    $('.xmark').click(function () {
+        $('#photo-back,#big').hide();
+    });
+
 });
